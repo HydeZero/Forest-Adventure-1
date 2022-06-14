e4c143817f014d1b981257ce4a20a4f4cceb8f64
HydeZero <95185597+HydeZero@users.noreply.github.com>
2022-06-14 13:11:08 -0500

    Update main.blocks, main.ts, images.g.jres, images.g.ts, tilemap.g.jres, tilemap.g.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,19 @@
+function playLevel () {
+    controller.moveSprite(mainCharacter, 50, 0)
+    if (controller.up.isPressed()) {
+        mainCharacter.vy = 10
+    }
+}
+function prepareLevel (levelNum: number) {
+    mainCharacter = sprites.create(assets.image`mainWalkR`, SpriteKind.Player)
+    tiles.setCurrentTilemap(tilemap`level1`)
+    playLevel()
+    if (levelNum == 1) {
+    	
+    }
+}
+let mainCharacter: Sprite = null
+scene.setBackgroundColor(0)
 music.playMelody("C E D F E G F A ", 600)
 story.printText("Welcome To...", 75, 60)
 story.printText("Forest", 75, 30, 7, 15, story.TextSpeed.VeryFast)
@@ -5,3 +21,8 @@ story.printText("Platformer", 75, 60, 2, 15, story.TextSpeed.VeryFast)
 story.printText("1", 75, 90, 8, 15, story.TextSpeed.VeryFast)
 let trackNumber = 1
 story.showPlayerChoices("Play", "Controls")
+if (story.checkLastAnswer("Play")) {
+    prepareLevel(1)
+} else {
+	
+}
