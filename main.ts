41f019107b7954263edd36cc407c1ea4e5467c2e
HydeZero <95185597+HydeZero@users.noreply.github.com>
2022-06-14 13:00:26 -0500

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,7 @@
+music.playMelody("C E D F E G F A ", 600)
+story.printText("Welcome To...", 75, 60)
+story.printText("Forest", 75, 30, 7, 15, story.TextSpeed.VeryFast)
+story.printText("Platformer", 75, 60, 2, 15, story.TextSpeed.VeryFast)
+story.printText("1", 75, 90, 8, 15, story.TextSpeed.VeryFast)
+let trackNumber = 1
+story.showPlayerChoices("Play", "Controls")
