2a0ce44170f6ac4e4507d2373a5bdde0b8d855f2
HydeZero <95185597+HydeZero@users.noreply.github.com>
2022-06-15 12:58:42 -0500

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,18 +1,35 @@
 function playLevel () {
+    levelFinish = 0
     controller.moveSprite(mainCharacter, 50, 0)
-    if (controller.up.isPressed()) {
-        mainCharacter.vy = 10
+    while (levelFinish == 0) {
+        // prevents game from CRASHING!
+        pause(100)
+        if (controller.up.isPressed()) {
+            vy = -20
+            mainCharacter.vy = vy
+            while (!(mainCharacter.isHittingTile(CollisionDirection.Bottom))) {
+                mainCharacter.vy += 1
+                pause(100)
+            }
+        }
     }
 }
 function prepareLevel (levelNum: number) {
+    scene.setBackgroundColor(8)
     mainCharacter = sprites.create(assets.image`mainWalkR`, SpriteKind.Player)
     tiles.setCurrentTilemap(tilemap`level1`)
+    // buffer
+    // 
+    pause(100)
+    scene.cameraFollowSprite(mainCharacter)
     playLevel()
     if (levelNum == 1) {
     	
     }
 }
+let vy = 0
 let mainCharacter: Sprite = null
+let levelFinish = 0
 scene.setBackgroundColor(0)
 music.playMelody("C E D F E G F A ", 600)
 story.printText("Welcome To...", 75, 60)
